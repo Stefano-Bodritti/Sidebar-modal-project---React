c4d6a881451d9e25768daa682c6789703885a023
Stefano <tepo@hotmail.it>
2023-03-20 00:22:41 +0100

    static page

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,39 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="coding addict" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="link">
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>
+                {link.icon}
+                {link.text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          return (
+            <li key={link.id}>
+              <a href={link.url}>
+                {link.icon}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 }
 
 export default Sidebar
